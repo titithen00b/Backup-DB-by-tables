1a8fb4cd61e8dc6527790b2adec6049e51106db3
Valentin Roche <54205303+titithen00b@users.noreply.github.com>
2023-12-27 11:03:03 +0100

    Update dumpdb.sh
    Add conditionnal creating tempfolder
    Minor change

--- dumpdb.sh
+++ dumpdb.sh
@@ -11,14 +11,20 @@ FOLDERBACKUP=
 #It's defines the maximum time (in days) to keep backup files before automatic delete
 TIMEDELETE=3
 
+if [ -d "$TEMPFOLDER ] ; then
+    echo "The temporary folder $TEMPFOLDER exist!"
+else 
+    echo "The temporary folder $TEMPFOLDER not exist! Creating this..."
+    mkdir -p $TEMPFOLDER
 
+fi
+    
 for i in $(mysql -u $DBUSER -h $DBHOST -p$DBPASSWD -P $DBPORT -D $DBBASE -e "show tables;")
-do mysqldump --no-tablespaces -h $DBHOST -u $DBUSER -p$DBPASSWD -P $DBPORT $DBBASE $i > $TEMPFOLDER/$i".sql"
+    do mysqldump --no-tablespaces -h $DBHOST -u $DBUSER -p$DBPASSWD -P $DBPORT $DBBASE $i > $TEMPFOLDER/$i".sql"
 done
 
 cd $FOLDERBACKUP
 tar -cjf "backup_wp_"$(date +'%Y%m%d')".tar.bz2" $TEMPFOLDER/*.sql
-cd $TEMPFOLDER
-rm *.sql
+rm -rf $TMPFOLDER/*.sql
 
 find $FOLDERBACKUP/ -name '*.tar.bz2' -mtime +$TIMEDELETE -exec rm {} \; 
